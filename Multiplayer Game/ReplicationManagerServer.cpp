fe36ce18928fda550f3406f606513ceafcefd9cd
Victor Chen <36187875+Scarzard@users.noreply.github.com>
2020-12-28 00:44:45 +0100

    Begining to fill write function from ReplicationManagerServer

--- Multiplayer Game/ReplicationManagerServer.cpp
+++ Multiplayer Game/ReplicationManagerServer.cpp
@@ -21,5 +21,49 @@ void ReplicationManagerServer::destroy(uint32 networkID)
 
 void ReplicationManagerServer::write(OutputMemoryStream& packet, ReplicationCommand action)
 {
+	for (std::map<uint32, ReplicationAction>::iterator it = rep_commands.begin(); it != rep_commands.end(); ++it)
+	{
+		if ((*it).second == ReplicationAction::Create)
+		{
+			packet << (*it).first;
+			packet << (*it).second;
 
+			GameObject* go = App->modLinkingContext->getNetworkGameObject((*it).first, false);
+
+			if (!go)
+			{
+				go = Instantiate();
+				go->networkId = (*it).first;
+				packet << 1;
+			}
+			else
+			{
+				packet << 0;
+			}
+
+			packet << go->position.x;
+			packet << go->position.y;
+			packet << go->size.x;
+			packet << go->size.y;
+			packet << go->angle;
+
+		}
+		else if ((*it).second == ReplicationAction::Update)
+		{
+			GameObject* go = App->modLinkingContext->getNetworkGameObject((*it).first);
+
+			packet << go->position.x;
+			packet << go->position.y;
+			packet << go->size.x;
+			packet << go->size.y;
+			packet << go->angle;
+
+			if (go->behaviour)
+				go->behaviour->write(packet);
+		}
+		ReplicationCommand command;
+		command.action = (*it).second;
+		command.networkID = (*it).first;
+	}
+	rep_commands.clear();
 }
