4331560c27f39ee07620a0a453b44dea450f28d8
Victor Chen <36187875+Scarzard@users.noreply.github.com>
2020-12-31 13:56:16 +0100

    Delivery Manager skeleton

--- Multiplayer Game/DeliveryManager.cpp
+++ Multiplayer Game/DeliveryManager.cpp
@@ -1,3 +1,45 @@
 #include "Networks.h"
 
 // TODO(you): Reliability on top of UDP lab session
+
+#include "DeliveryManager.h"
+
+DeliveryManager::~DeliveryManager()
+{
+	clear();
+}
+
+Delivery* DeliveryManager::writeSequenceNumber(OutputMemoryStream& packet)
+{
+
+}
+
+bool DeliveryManager::processSequenceNumber(const InputMemoryStream& packet)
+{
+
+}
+
+bool DeliveryManager::hasSequenceNumberspendingAck() const
+{
+
+}
+
+void DeliveryManager::writeSequenceNumbersPendingAck(OutputMemoryStream& packet)
+{
+
+}
+
+void DeliveryManager::processAckdSequenceNumbers(const InputMemoryStream& packet)
+{
+
+}
+
+void DeliveryManager::processTimedOutPackets()
+{
+
+}
+
+void DeliveryManager::clear()
+{
+
+}
--- Multiplayer Game/DeliveryManager.h
+++ Multiplayer Game/DeliveryManager.h
@@ -1,3 +1,52 @@
 #pragma once
 
 // TODO(you): Reliability on top of UDP lab session
+
+class DeliveryManager;
+
+class DeliveryDelegate
+{
+public:
+	virtual void onDeliverySuccess(DeliveryManager* deliveryManager) = 0;
+	virtual void onDeliveryFailure(DeliveryManager* deliveryManager) = 0;
+};
+
+struct Delivery
+{
+	uint32 sequenceNumber = 0;
+	double startTime = 0.0;
+	DeliveryDelegate* delegate = nullptr;
+
+};
+
+class DeliveryManager
+{
+public:
+	~DeliveryManager();
+
+	//For senders to write a new seq. numbers into a packet
+	Delivery* writeSequenceNumber(OutputMemoryStream& packet);
+
+	//For recievers to process the seq. number from an incoming packet
+	bool processSequenceNumber(const InputMemoryStream& packet);
+
+	//For recievers to write ack'ed seq. numbers into a packet
+	bool hasSequenceNumberspendingAck() const;
+	void writeSequenceNumbersPendingAck(OutputMemoryStream& packet);
+
+	//For senders to process ack'ed seq. numbers from a packet
+	void processAckdSequenceNumbers(const InputMemoryStream& packet);
+	void processTimedOutPackets();
+
+	void clear();
+
+
+private:
+	// Sender side
+	uint32	NextOutgoingSequenceNumber = 0;
+	std::vector<Delivery*> PendingDeliveries;
+
+	//Receiver side
+	uint32	NextExpectedSequenceNumber = 0;
+	std::vector<uint32> PendingAcks;
+};
